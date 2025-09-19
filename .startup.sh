83c707527139fb4f783f50b02539bbda0b32f404
drkdev <darrenkwondev46@gmail.com>
2025-09-20 07:29:41 +0900

    Update .startup.sh

--- .startup.sh
+++ .startup.sh
@@ -6,6 +6,9 @@ sudo apt update && sudo apt install -y build-essential git curl wget vim tree un
 # bitcoin daemon dependencies (https://github.com/bitcoin/bitcoin/blob/master/doc/dependencies.md)
 sudo apt install -y autotools-dev automake bsdmainutils python3 libevent-dev libminiupnpc-dev libzmq3-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler capnproto libcapnp-dev pkgconf systemtap-sdt-dev
 
+# sys check  
+sudo apt install -y smartmontools sysstat  
+
 # pick full install or partial install what you need
 sudo apt install -y libboost-all-dev
 sudo apt install -y libboost-system-dev libboost-filesystem-dev libboost-test-dev libboost-thread-dev
