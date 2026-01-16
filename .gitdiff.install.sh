c85187d390d9c1efc18358a92ba105692fde1fdd
drkup <darrenkwondev46@gmail.com>
2026-01-17 06:26:14 +0900

    tmux, gitdiff delta

---
+++ .gitdiff.install.sh
@@ -0,0 +1,8 @@
+# brew install git-delta
+git config --global core.pager "delta"
+git config --global interactive.diffFilter "delta --color-only"
+git config --global delta.navigate true
+git config --global delta.side-by-side true
+git config --global delta.line-numbers true
+git config --global delta.syntax-theme Monokai
+git config --global diff.algorithm histogram
